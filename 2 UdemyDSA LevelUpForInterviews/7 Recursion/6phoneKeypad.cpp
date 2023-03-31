149c498bb314507a79a63701b09541365511284d
sarthak <sarthakbhardwaj713@gmail.com>
2023-03-31 11:36:27 +0530

    phone keypad

--- 2 UdemyDSA LevelUpForInterviews/7 Recursion/6phoneKeypad.cpp
+++ 2 UdemyDSA LevelUpForInterviews/7 Recursion/6phoneKeypad.cpp
@@ -1,34 +1,46 @@
 #include<iostream>
+#include<vector>
 using namespace std;
 
 /*
+Leetcode 17
+
 Phone Keypad:
 - Given a number n, modern keypad
 - find all possible strings
 - generated using that number
 */
 
-string keypad[]={"","","ABC","DEF","GHI","JKL","MNO","PQRS","TUV","WXYZ"};
+string keypad[]={"","","abc","def","ghi","jkl","mno","pqrs","tuv","wxyz"};
 
-void printKeypadOutput(string input,string output,int i){
-    if(input[i]=='\0'){
-        cout<<output<<endl;
+void solve(string digits,string output,int index,vector<string>&ans){
+    if(index>=digits.size()){
+        ans.push_back(output);
         return;
     }
-    int currDigit=input[i]-'0';
-    if(currDigit==0 || currDigit==1){
-        printKeypadOutput(input,output,i+1);
+    int num=digits[index]-'0';
+    string s=keypad[num];
+    for(int i=0;i<s.size();i++){
+        solve(digits,output+s[i],index+1,ans);
+    } 
+}
+
+vector<string> letterCombinations(string digits){
+    vector<string>ans;
+    if(digits.size()==0){
+        return ans;
     }
-    for(int j=0;j<keypad[currDigit].size();j++){
-        printKeypadOutput(input,output+keypad[currDigit][j],i+1);
-    }
+    solve(digits,"",0,ans); 
+    return ans;
 }
 
 int main(){
     
-    string input="234";
+    string digits="23";
     string output="";
-    printKeypadOutput(input,output,0);
+    vector<string>ans=letterCombinations(digits);
+    for(auto i:ans)
+        cout<<i<<endl;
     
     return 0;
 }

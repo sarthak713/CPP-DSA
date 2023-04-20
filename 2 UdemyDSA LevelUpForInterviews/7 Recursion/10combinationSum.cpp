f150ffd59f904623ddb78c7a9b4cb9e3ed010ae7
sarthak <sarthakbhardwaj713@gmail.com>
2023-04-20 13:39:50 +0530

    recursion combination sum

---
+++ 2 UdemyDSA LevelUpForInterviews/7 Recursion/10combinationSum.cpp
@@ -0,0 +1,47 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+/*
+Combination Sum (Leetcode 39)
+
+- Given array, & target integer
+- return list of all unique combinations of candidates
+- where chosen numbers sum to target
+- Same number may be chosen an unlimited number of times
+*/
+
+void solve(int i,int sum,vector<int>arr,vector<vector<int>>&ans,vector<int>output,int target){
+    if(sum>=target || i>=arr.size()){
+        if(sum==target){
+            ans.push_back(output);
+        }
+        return;
+    }
+    output.push_back(arr[i]);
+    solve(i,sum+arr[i],arr,ans,output,target);
+    output.pop_back();
+    solve(i+1,sum,arr,ans,output,target);
+}
+
+vector<vector<int>>combinationSum(vector<int>&arr,int target){
+    vector<vector<int>>ans;
+    vector<int>output;
+    solve(0,0,arr,ans,output,target);
+    return ans;
+}
+
+int main(){
+    
+    vector<int>arr={2,3,5};
+    int target=8;
+    // ans = {2,2,3} {7}
+    vector<vector<int>>ans=combinationSum(arr,target);
+    for(auto i:ans){
+        for(auto j:i){
+            cout<<j<<" ";
+        }cout<<endl;
+    }
+    
+    return 0;
+}

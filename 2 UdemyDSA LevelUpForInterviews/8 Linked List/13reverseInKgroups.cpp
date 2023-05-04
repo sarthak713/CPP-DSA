a481253ba7e000183678900257080d1e4271edaa
sarthak <sarthakbhardwaj713@gmail.com>
2023-05-04 11:47:06 +0530

    reverse k

--- 2 UdemyDSA LevelUpForInterviews/8 Linked List/13reverseInKgroups.cpp
+++ 2 UdemyDSA LevelUpForInterviews/8 Linked List/13reverseInKgroups.cpp
@@ -1,6 +1,14 @@
 #include<iostream>
 using namespace std;
 
+/*
+Reverse in K Groups
+
+Approach:
+    - Reverse K starting nodes & recursively reverse others
+
+*/
+
 class node{
 public:
     int data;
@@ -14,7 +22,7 @@ void insertAtTail(node*&head,int data){
     node*n=new node(data);
     if(head==NULL){
         head=n;
-        return; 
+        return;
     }
     node*temp=head;
     while(temp->next!=NULL){
@@ -23,35 +31,32 @@ void insertAtTail(node*&head,int data){
     temp->next=n;
 }
 void print(node*head){
-    if(head==NULL){
-        cout<<"No Element in List: NULL"<<endl;
-        return;
-    }
-    node*temp=head;
+    node*temp=head;  
     while(temp!=NULL){
         cout<<temp->data<<" ";
         temp=temp->next;
     }cout<<"NULL"<<endl;
 }
 
-// Reverse nodes in K Groups
-node* kReverse(node*head,int k){
+// Reverse in K groups
+
+node*reverseK(node*&head,int k){
     if(head==NULL || head->next==NULL){
         return head;
     }
-    node*prev=NULL;
     node*curr=head;
-    node*forward=NULL;
-    int count=1;
-    while(curr!=NULL && count<=k){
-        forward=curr->next;
+    node*prev=NULL;
+    node*next=NULL;
+    int count=0;
+    while(curr!=NULL && count<k){
+        next=curr->next;
         curr->next=prev;
         prev=curr;
-        curr=forward;
+        curr=next;
         count++;
     }
-    if(forward!=NULL){
-        head->next=kReverse(forward,k);
+    if(next!=NULL){
+        head->next=reverseK(curr,k);
     }
     return prev;
 }
@@ -63,12 +68,10 @@ int main(){
     insertAtTail(head,3);
     insertAtTail(head,4);
     insertAtTail(head,5);
-    insertAtTail(head,6);
-    insertAtTail(head,7);
-    insertAtTail(head,8);
-    int k=3;
-    node*newList=kReverse(head,3);
-    print(newList);
+    print(head);
+    node*newHead=reverseK(head,3);
+    print(newHead);
+
     
     return 0;
 }
